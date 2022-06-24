170926775ebbd8c39d084d9360feebedb8645f0c
ArtemBobr <welsPGSj@gmail.com>
2022-06-24 14:32:23 +0300

    Fixed encoding

--- HomeWork_3/src/com/ArtemBobr/HomeWork_3/HomeWork3_1.java
+++ HomeWork_3/src/com/ArtemBobr/HomeWork_3/HomeWork3_1.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class HomeWork3_1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println(""Введите число: ");
+        System.out.println("Введите число: ");
         int i = sc.nextInt();
         if(isThreeDigit(i)){
             System.out.println("Число трёхзначное: " + isThreeDigit(i));
